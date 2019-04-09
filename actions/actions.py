c7a15c101c3566a34e24d384486c74217c32d2c9
realhum <34401244+realhum@users.noreply.github.com>
2019-04-09 05:32:49 +0300

    Add files via upload

--- actions/actions.py
+++ actions/actions.py
@@ -18,10 +18,24 @@ from rasa_core_sdk.events import SlotSet
 
 logger = logging.getLogger(__name__)
 
-def get_time():
-    ts = time.time()
-    return ts
+class StalkerAnecdote(Action):
+    def name(self):
+        return 'tell_an_anecdote'
+    def run(self, dispatcher, tracker, domain):
+        theme = tracker.get_slot('anecdote_theme')
+        dispatcher.utter_message('There should be funny anecdote with ' + theme)
+        dispatcher.utter_message('But take this instead')
+        buttons = []
+        laugh = ""
+        for i in range(3):
+            laugh = (laugh + "ha")
+            title = (laugh)
+            payload = ("/laugh")
+            buttons.append({"title": title, "payload": payload})
+        dispatcher.utter_button_template('utter_joke', buttons,tracker)
+        return []
 
+'''
 class MemoryVisit(Action):
     def name(self):
         return "memory_visit"
@@ -53,7 +67,8 @@ class MemoryVisit(Action):
             dispatcher.utter_message("Oooh, I remember your smily face. It was sooo long ago. Thanks Gods I still remember correct time. I've met you %s" % first_time)
         connection.close()
         return []
-    
+'''
+
 class AnswerQuestion(Action):
     def name(self):
         return "answer_question"
@@ -61,5 +76,92 @@ class AnswerQuestion(Action):
     def run(self, dispatcher, tracker, domain):
         # what your action should do
         info = tracker.get_slot('info')
-        dispatcher.utter_message("Yea, I can tell you a lot of things about %s" % info)
+        if(info is None):
+            dispatcher.utter_message("Okey, what things do you want to find out about?")
+        else:    
+            dispatcher.utter_message("Yea, I can tell you a lot of things about %s" % info)
+        return []
+
+
+class ActionFindHideaway(Action):
+    def name(self):
+        return "action_find_hideaway"
+
+    def run(self, dispatcher, tracker, domain):
+        connection = MongoClient("ds127376.mlab.com", 27376)
+        db = connection["chatbot"]
+        db.authenticate("rasaguy", "rasabot1")
+        collection = db['stations']
+
+        stations = [document['station_name'] for document in collection.find()]
+        selected_station = random.choice(stations)
+        dispatcher.utter_message("You can hide in {}".format(selected_station))
+        connection.close()
+
+        return []
+
+
+class ActionCheckHideaway(Action):
+    def name(self):
+        return "action_check_hideaway"
+
+    def run(self, dispatcher, tracker, domain):
+        is_can = random.choice([True, False])
+        if is_can:
+            dispatcher.utter_template("utter_can_hide", tracker)
+        else:
+            dispatcher.utter_template("utter_cant_hide", tracker)
+
+        return []
+
+
+class ActionLastEmission(Action):
+    def name(self):
+        return "action_last_emission"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message("Last emission was {} minutes ago".format(datetime.datetime.today().minute))
+        return []
+
+
+class ActionFutureEmission(Action):
+    def name(self):
+        return "action_future_emission"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message("The emission will be in {} minutes".format(60 - datetime.datetime.today().minute))
+        return []
+
+
+class ActionBuy(Action):
+    def name(self):
+        return "action_buy"
+
+    def run(self, dispatcher, tracker, domain):
+        # what your action should do
+        money = tracker.get_slot('money')
+        if (money is None):
+            dispatcher.utter_message("How much money do you have?")
+        else:
+            dispatcher.utter_message("You have %s rubles and you can buy canned meat" % money)
+        return []
+
+
+class ActionBuyCost(Action):
+    def name(self):
+        return "action_buy_cost"
+
+    def run(self, dispatcher, tracker, domain):
+        # what your action should do
+        money = tracker.get_slot('money')
+        dispatcher.utter_message("You have %s rubles and you can buy sausage and bread" % money)
+        return []
+
+
+class ActionSleep(Action):
+    def name(self):
+        return "action_sleep"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message("Can not help with this, look elsewhere.")
         return []
